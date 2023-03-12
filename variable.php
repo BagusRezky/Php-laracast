4f27de9da5a36788e6fd61a314c5cd8e346f5e14
BagusRezky <bagusrezky15@gmail.com>
2023-03-13 01:01:11 +0700

    Learn Variable in Php

--- variable.php
+++ variable.php
@@ -4,9 +4,21 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Learn Variable in Php</title>
 </head>
 <body>
     
+    <h1>
+        <?php
+            $text = "Php";
+            echo "I learn $text! <br>"; 
+
+            $text = "Rezky";
+            echo $text . " is my name <br>";
+
+            echo "Hello" . " World";
+        ?>
+    </h1>
+
 </body>
 </html>
