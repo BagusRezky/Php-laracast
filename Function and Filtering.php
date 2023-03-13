276150906e91f7a8d1913f0281b58bb08e6b7c4d
BagusRezky <bagusrezky15@gmail.com>
2023-03-13 09:50:25 +0700

    Learn Function and Filtering

--- Function and Filtering.php
+++ Function and Filtering.php
@@ -7,6 +7,50 @@
     <title>Function and Filtering</title>
 </head>
 <body>
-    
+    <?php
+    $books = [
+        [
+            'name' => 'Do Androids Dreams of Electric Sheep',
+            'author' => 'Philip K. Dick',
+            'releaseYear' => 1968,
+            'purchaseUrl' => 'http://example.com',
+        ],
+        [
+            'name' => 'Project Hail Mary',
+            'author' => 'Andy Weir',
+            'releaseYear' => 2021,
+            'purchaseUrl' => 'http://example.com',
+        ],
+        [
+            'name' => 'The Martian',
+            'author' => 'Andy Weir',
+            'releaseYear' => 2011,
+            'purchaseUrl' => 'http://example.com',
+        ],
+    ];
+
+    function filterByAuthor($books){
+        $filteredBook = [];
+
+        foreach($books as $book){
+            if ($book['author'] == 'Andy Weir') {
+                $filteredBook[] = $book;
+            }
+        }
+
+        return $filteredBook;
+        
+    }
+    ?>
+
+    <ul>
+        <?php foreach (filterByAuthor($books) as $book) : ?>
+            <li>
+                <a href="<?= $book['purchaseUrl'] ?>">
+                <?= $book['name']; ?>(<?= $book['releaseYear']?>) - By <?= $book['author']?>
+            </a>
+            </li>
+        <?php endforeach; ?>
+    </ul>
 </body>
 </html>
