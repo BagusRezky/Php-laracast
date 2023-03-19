d81bb27646c4ef767528d928eb80abe80ef5feb9
BagusRezky <bagusrezky15@gmail.com>
2023-03-19 15:07:36 +0700

    Learn Php logic

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+$books = [
+    [
+        'name' => 'Do Androids Dreams of Electric Sheep',
+        'author' => 'Philip K. Dick',
+        'releaseYear' => 1968,
+        'purchaseUrl' => 'http://example.com',
+    ],
+    [
+        'name' => 'Project Hail Mary',
+        'author' => 'Andy Weir',
+        'releaseYear' => 2021,
+        'purchaseUrl' => 'http://example.com',
+    ],
+    [
+        'name' => 'The Martian',
+        'author' => 'Andy Weir',
+        'releaseYear' => 2011,
+        'purchaseUrl' => 'http://example.com',
+    ],
+];
+
+$filteredBooks = array_filter($books,function($book){
+    return $book['releaseYear'] > 2000;
+});
+require "index.view.php";
